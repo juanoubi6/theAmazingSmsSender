cabaf63d908216185d69c427e5660c112af23ab0
Juan Manuel Oubiña <juan.manuel.oubina@litebyte.us>
2018-08-29 18:37:56 -0300

    Add delay

--- app/rabbitMqManager.go
+++ app/rabbitMqManager.go
@@ -9,6 +9,7 @@ import (
 	"theAmazingSmsSender/app/config"
 	"theAmazingSmsSender/app/helpers/twilio"
 	"theAmazingSmsSender/app/communications/rabbitMQ/tasks"
+	"time"
 )
 
 var workerAmount, _ = strconv.Atoi(config.GetConfig().WORKER_AMOUNT)
@@ -188,6 +189,7 @@ func consumeSmsQueueWorker(number int){
 
 	for d := range msgs {
 		println("Received SMS task on queue: " + strconv.Itoa(number))
+		time.Sleep(time.Millisecond*200)
 		/*var smsMessageData tasks.SmsMessage
 		err := json.Unmarshal(d.Body, &smsMessageData)
 		if err != nil {
